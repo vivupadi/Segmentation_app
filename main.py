b30cec6360a3f016fe8098c18e18d74eff2762b5
vivupadi <vivupadi@gmail.com>
2024-10-28 00:17:31 +0100

    Flask API

---
+++ main.py
@@ -0,0 +1,22 @@
+import torch
+from flask import Flask
+import numpy as np
+
+import Qt
+
+app = Flask(__name__)
+
+#Load models
+#model =
+
+
+#Load image 
+#def preprocess(image)
+
+@app.route('/predict', method=[POST])
+
+def predict():
+    return label
+
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0', port = 5000)
