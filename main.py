629b930fd389f6340714cd0e9260a666c283174c
vivupadi <vivupadi@gmail.com>
2024-10-29 23:54:18 +0100

    Flast update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import torch
-from flask import Flask
+from flask import Flask, jsonify, request
+from PIL import Image
 import numpy as np
 
 import Qt
@@ -11,12 +12,22 @@ app = Flask(__name__)
 
 
 #Load image 
-#def preprocess(image)
+def preprocess(image):
+    image = image.resize(224,224)
+    image = np.array(image)/255
+    image = np.expand_dims(image, axis= 0)
+    return image
+    
 
-@app.route('/predict', method=[POST])
+@app.route('/predict', methods=['POST'])
 
 def predict():
-    return label
+    file = request.files['file']
+    image = Image.open(file)
+    image = preprocess(image)
+
+    prediction = model.predict(image)
+    return jsonify({predictions: prediction.tolist()})
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 5000)
