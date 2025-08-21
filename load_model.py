826f7079e6e10808de2319ddb93a47e4c96eb859
vivupadi <vivupadi@gmail.com>
2025-08-21 23:50:19 +0200

    Half through video conversion

---
+++ load_model.py
@@ -0,0 +1,40 @@
+import torch
+
+from preprocess import *
+
+
+model = torch.hub.load('pytorch/vision:v0.10.0', 'deeplabv3_mobilenet_v3_large', pretrained=True)
+model.eval()
+#print(model)
+
+
+####Test file###
+# Download an example image from the pytorch website
+#import urllib
+#url, filename = ("https://github.com/pytorch/hub/raw/master/images/deeplab1.png", "deeplab1.png")
+##try: urllib.URLopener().retrieve(url, filename)
+#except: urllib.request.urlretrieve(url, filename)
+################
+
+#import glob
+#from PIL import Image
+#filename = "C:\\Users\\Vivupadi\\Desktop\\Portfolio\\img\\section_2.jpg"
+filename = "C:\\Users\\Vivupadi\\Downloads\\PXL_20250802_072807827.mp4"
+
+
+
+#output_predictions = preprocess_image(filename, model)
+output_predictions = preprocess_video(filename, model)
+
+
+mask_image(filename, output_predictions)
+
+dummy_input = torch.randn(1, 3, 224, 224)
+
+# Export to ONNX
+torch.onnx.export(
+    model, 
+    dummy_input, 
+    "deeplabv3_mobilenet.onnx",
+    opset_version=11
+)
