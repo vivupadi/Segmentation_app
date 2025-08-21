4b38a79705e6ee136d580f161e627745f969e3d1
vivupadi <vivupadi@gmail.com>
2025-08-22 01:14:08 +0200

    working segmentation but framewise

--- preprocess.py
+++ preprocess.py
@@ -3,6 +3,8 @@ import torch
 from PIL import Image
 from torchvision import transforms
 
+import matplotlib.pyplot as plt
+
 import cv2
 import numpy as np
 import onnxruntime as ort
@@ -29,18 +31,18 @@ def preprocess_image(filename, model):
 
 def preprocess_video(filename, model):
     video = cv2.VideoCapture(filename)
-    ret, fFrame = video.read()
 
     while video.isOpened():
         ret, fFrame = video.read()
         if not ret:
             break
         
-        input_image = cv2.resize(fFrame, (224, 224))
+        input_image = cv2.cvtColor(fFrame, cv2.COLOR_BGR2RGB)
+        input_image = cv2.resize(input_image, (224, 224))
         #input_image = Image.open(filename)
 
         #get frames
-        input_image = input_image.convert("RGB")
+        #input_image = input_image.convert("RGB")
         preprocess = transforms.Compose([
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
@@ -54,7 +56,10 @@ def preprocess_video(filename, model):
 
         with torch.no_grad():
             output = model(input_batch)['out'][0]
-        return mask_image(filename, output.argmax(0))
+            mask_video(fFrame, output.argmax(0))
+    video.release()
+    cv2.destroyAllWindows()
+    #return mask_video(fFrame, output.argmax(0))
 
 
 def mask_image(filename, output_predictions):
@@ -70,5 +75,28 @@ def mask_image(filename, output_predictions):
     r.putpalette(colors)
 
     import matplotlib.pyplot as plt
+    plt.imshow(r)
+    plt.show()
+
+
+#Masking video
+def mask_video(fFrame, output_predictions):
+    #video = cv2.VideoCapture(filename)
+    #ret, fFrame = video.read()
+
+    input_image = cv2.cvtColor(fFrame, cv2.COLOR_BGR2RGB)
+    input_image = cv2.resize(input_image, (224, 224))
+    # create a color pallette, selecting a color for each class
+    palette = torch.tensor([2 ** 25 - 1, 2 ** 15 - 1, 2 ** 21 - 1])
+    colors = torch.as_tensor([i for i in range(21)])[:, None] * palette
+    colors = (colors % 255).numpy().astype("uint8")
+    breakpoint()
+    h,w, _ = fFrame.shape
+    # plot the semantic segmentation predictions of 21 classes in each color
+    mask = output_predictions.byte().cpu().numpy()
+    r = Image.fromarray(mask).resize((w,h))
+
+    r.putpalette(colors)
+
     plt.imshow(r)
     plt.show()
