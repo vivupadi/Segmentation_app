3636ad15ba6d16906355a43e9165c36250777f4d
vivupadi <vivupadi@gmail.com>
2025-08-23 13:33:49 +0200

    Realtime video segmentation done on Laptop based on given video mp4 file

--- preprocess.py
+++ preprocess.py
@@ -57,6 +57,8 @@ def preprocess_video(filename, model):
         with torch.no_grad():
             output = model(input_batch)['out'][0]
             mask_video(fFrame, output.argmax(0))
+        
+        #plt.show()
         # Press Q to quit
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
@@ -84,34 +86,35 @@ def mask_image(filename, output_predictions):
 
 #Masking video
 def mask_video(fFrame, output_predictions):
-    #video = cv2.VideoCapture(filename)
-    #ret, fFrame = video.read()
 
     input_image = cv2.cvtColor(fFrame, cv2.COLOR_BGR2RGB)
     input_image = cv2.resize(input_image, (224, 224))
+
     # create a color pallette, selecting a color for each class
     palette = torch.tensor([2 ** 25 - 1, 2 ** 15 - 1, 2 ** 21 - 1])
     colors = torch.as_tensor([i for i in range(21)])[:, None] * palette
     colors = (colors % 255).numpy().astype("uint8")
-    breakpoint()
-    h,w, _ = fFrame.shape
+
     # plot the semantic segmentation predictions of 21 classes in each color
     mask = output_predictions.byte().cpu().numpy()
-    r = Image.fromarray(mask).resize((w,h))
 
     # Apply color map for visualization
-    #color_mask = np.zeros_like(fFrame)
-    #color_mask[mask == 15] = [0, 255, 0]   # Example: person=green (class 15)
+    mask_color = np.zeros((224, 224, 3), dtype=np.uint8)
 
-    breakpoint()
+    #Resize original frame
+    fFrame_resize = cv2.resize(fFrame, (224,224), interpolation=cv2.INTER_NEAREST)
+    #breakpoint()
+
+    # Map empty mask array with the coresponding class as per prediction
+    for class_id in np.unique(mask):
+        mask_color[mask == class_id] = colors[class_id]
+   
+    #breakpoint()
     # Overlay mask on original frame
-    overlay = cv2.addWeighted(fFrame, 0.7, colors, 0.3, 0)
+    overlay = cv2.addWeighted(fFrame_resize, 0.6, mask_color, 0.7, 0)
 
-    breakpoint()
-    # Show live result
+
+    cv2.namedWindow("Segmentation", cv2.WINDOW_NORMAL)
+    cv2.setWindowProperty("Segmentation", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
     cv2.imshow("Segmentation", overlay)
-
-    #r.putpalette(colors)
-
-    #plt.imshow(r)
-    #plt.show()
+   
